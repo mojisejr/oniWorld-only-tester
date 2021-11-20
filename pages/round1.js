c44e404e41910015a45b13edd741cadbb75d90d8
nonthasak laoluerat <nonthasak.l@Appels-iMac.local>
2021-11-20 17:26:54 +0700

    round1 implements

--- pages/round1.js
+++ pages/round1.js
@@ -2,27 +2,50 @@ import Web3 from "web3";
 import detectEthereumProvider from "@metamask/detect-provider";
 import { useEffect, useState } from "react";
 import { abi, address } from "./smartcontract/oni";
+import styles from "../styles/oniTest.module.css";
 
 function OniTestRound1() {
   //round state
-  const maxLevel1Count = 2;
-  const [ethereum, setEthereum] = useState();
   const [account, setAccount] = useState();
   const [contract, setContract] = useState();
-  const [canTestOni, setCanTestOni] = useState();
+  const [web3, setWeb3] = useState();
+  const [canTestOni, setCanTestOni] = useState([]);
+  const [connected, setConnected] = useState(false);
+  const [loading, isLoading] = useState(false);
+  const [token1, setToken1] = useState();
+  const [token2, setToken2] = useState();
+  const [result, setResult] = useState(false);
+
+  useEffect(() => {
+    if (!account || !contract || !web3) {
+      const web3 = new Web3(ethereum);
+      const contract = new web3.eth.Contract(abi, address);
+      setConnected(false);
+      setWeb3(web3);
+      setContract(contract);
+      console.log("Please Connect wallet.");
+    } else {
+      setConnected(true);
+    }
+    if (connected) {
+      fetchAllOni();
+    }
+  }, [connected]);
 
   async function connect() {
-    const provider = detectEthereumProvider();
-    if (provider) {
-      const web3 = new Web3(provider);
-      const contract = new web3.eth.Contract(abi, address);
-      setContract(contract);
-      setEthereum(provider);
-    }
-    const accounts = await provider.request({ method: "eth_requestAccounts" });
-    setAccount(accounts[0]);
-    if (!(await isTestOpen())) {
-      alert("Testing is not open yet. please wait.");
+    try {
+      const ethereum = await detectEthereumProvider();
+      if (!ethereum) {
+        return;
+      }
+      ethereum.request({ method: "eth_requestAccounts" }).then((accounts) => {
+        if (accounts.length > 0) {
+          setAccount(accounts[0]);
+          setConnected(true);
+        }
+      });
+    } catch (e) {
+      console.log(e.message);
     }
   }
 
@@ -31,15 +54,17 @@ function OniTestRound1() {
   }
 
   async function fetchAllOni() {
+    isLoading(true);
     const oniInWallet = await contract.methods.balanceOf(account).call();
     let fetchedOni = [];
     //fetch tokenIds from wallet
     for (let i = 0; i < +oniInWallet; i++) {
-      fetchOni.push(
+      fetchedOni.push(
         +(await contract.methods.tokenOfOwnerByIndex(account, i).call())
       );
     }
     await getAvailableOni(fetchedOni);
+    isLoading(false);
   }
 
   async function getAvailableOni(oni) {
@@ -49,11 +74,11 @@ function OniTestRound1() {
     }
     for (let i = 0; i < oni.length; i++) {
       const oniLevel = await contract.methods.oni(i).call();
-      if (+oniLevel != 0) {
+      if (+oniLevel == 0) {
         validOni.push(oni[i]);
       }
     }
-    setCanTestOni(oni);
+    setCanTestOni(validOni);
   }
 
   async function Test(oniArray) {
@@ -68,12 +93,93 @@ function OniTestRound1() {
       });
   }
 
+  async function enterMultiTesting() {
+    const tokenArray = [token1, token2];
+    if (!(await isTestOpen())) {
+      alert("Testing is closed");
+    } else {
+      await Test(tokenArray);
+    }
+  }
+
+  async function enterSingleTesting() {
+    if (!(await isTestOpen())) {
+      alert("Testing is closed");
+    } else {
+      await Test(tokenArray);
+    }
+  }
+
+  function twoTokenProvider() {
+    if (canTestOni.length > 0) {
+      return (
+        <div>
+          <select
+            className="token-1"
+            onChange={(e) => setToken1(e.target.value)}
+          >
+            <option>select first token</option>
+            {canTestOni
+              .filter((oni) => oni != token2)
+              .map((oni) => (
+                <option value={oni} key={oni}>
+                  token ID: #{oni}
+                </option>
+              ))}
+          </select>
+          <select
+            className="token-2"
+            onChange={(e) => setToken2(e.target.value)}
+          >
+            <option>select second token</option>
+            {canTestOni
+              .filter((oni) => oni != token1)
+              .map((oni) => (
+                <option key={oni} value={oni}>
+                  token ID: #{oni}
+                </option>
+              ))}
+          </select>
+          <button onClick={enterMultiTesting}>GO!</button>
+        </div>
+      );
+    } else {
+      return <div>no Oni in your account</div>;
+    }
+  }
+
+  function singleTokenProvider() {
+    if (canTestOni.length > 0) {
+      return (
+        <div>
+          <select
+            className="token-1"
+            onChange={(e) => setToken1(e.target.value)}
+          >
+            <option>select token</option>
+            {canTestOni.map((oni) => (
+              <option value={oni} key={oni}>
+                token ID: #{oni}
+              </option>
+            ))}
+          </select>
+          <button onClick={enterSingleTesting}>GO!</button>
+        </div>
+      );
+    }
+  }
+
   return (
     <div>
       <button onClick={connect}>Connect wallet</button>
-      <button onClick={isTestOpen}>IsTestOpen</button>
-      <button onClick={fetchAllOni}>Fetch Oni</button>
-      <button onClick={Test}>Test</button>
+      <div className={styles.tokenProvidingBox}>
+        <div className="multi-token-box">
+          {loading ? <div>loading..</div> : twoTokenProvider()}
+        </div>
+        <div className="single-token-box">
+          {loading ? <div>loading..</div> : singleTokenProvider()}
+        </div>
+      </div>
     </div>
   );
 }
